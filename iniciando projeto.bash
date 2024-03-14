25cfbadf0466a92c274bde064d2695e06b98e86e
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-03-14 17:40:25 -0300

    Add: Iniciando projeto

---
+++ iniciando projeto.bash
@@ -0,0 +1,14 @@
+#Vai criar as pastas do DBT
+python -m venv dbt_venv
+#vai liberar o powerShell para funcionar a próxima linha
+Set-ExecutionPolicy RemoteSigned
+#caso não funciona ativar executar como administrador
+
+.\dbt_venv\Scripts\Activate.ps1
+
+#instalar o bdt-{banco de dados}
+pip install dbt-postgres
+
+#criar a pasta .dbt
+#no explorer digite %userprofile% (caira na pasta de perfil do usuário) e cria a pasta ".dbt"
+
